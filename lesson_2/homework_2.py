246565ae887a3577354b74bec358e99bc9e854f6
slaschinin.s <sem63sam@yandex.ru>
2022-02-19 18:32:21 +0400

    upd homework_2.py, there were questions

--- lesson_2/homework_2.py
+++ lesson_2/homework_2.py
@@ -8,3 +8,37 @@
 # По желанию можно добавить ещё параметры вакансии (например, работодателя и расположение).
 # Структура должна быть одинаковая для вакансий с обоих сайтов. Сохраните результат в json-файл
 
+from bs4 import BeautifulSoup as bs
+import requests
+from pprint import pprint
+
+
+url = 'https://hh.ru/search/vacancy'
+params = {'text': 'python',
+          'page': 0}
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'}
+
+response = requests.get(url, params=params, headers=headers)
+
+dom = bs(response.text, 'html.parser')
+
+vacancy_list = dom.find_all('div', {'class': 'vacancy-serp-item vacancy-serp-item_redesigned'})
+
+
+# pprint(vacancy_list)
+
+all_vacancy = []
+for vacancy in vacancy_list:
+    vacancy_data = {}
+    name = vacancy.find('a', {'data-qa': 'vacancy-serp__vacancy-title'}).getText()
+    link = vacancy.find('a', {'class': 'bloko-link'})['href']
+
+    vacancy_data['name'] = name
+    vacancy_data['link'] = link
+
+    all_vacancy.append(vacancy_data)
+
+pprint(all_vacancy)
+
+
