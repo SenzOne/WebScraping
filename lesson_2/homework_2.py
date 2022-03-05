afc44959f71b75058c49150840cdb6aa841c5fc5
slaschinin.s <sem63sam@yandex.ru>
2022-03-06 01:45:44 +0400

    the last commit was with errors, the belly is correct

--- lesson_2/homework_2.py
+++ lesson_2/homework_2.py
@@ -8,37 +8,111 @@
 # По желанию можно добавить ещё параметры вакансии (например, работодателя и расположение).
 # Структура должна быть одинаковая для вакансий с обоих сайтов. Сохраните результат в json-файл
 
-from bs4 import BeautifulSoup as bs
 import requests
-from pprint import pprint
+from bs4 import BeautifulSoup
+from dataclasses import dataclass
+import pandas as pd
+from time import sleep
 
 
-url = 'https://hh.ru/search/vacancy'
-params = {'text': 'python',
-          'page': 0}
-
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'}
-
-response = requests.get(url, params=params, headers=headers)
-
-dom = bs(response.text, 'html.parser')
-
-vacancy_list = dom.find_all('div', {'class': 'vacancy-serp-item vacancy-serp-item_redesigned'})
+@dataclass
+class Vacancy:
+    name = str
+    domain = str
+    salary = str
+    min_salary = int
+    max_salary = int
+    currency = str
+    site = str
 
 
-# pprint(vacancy_list)
+    def get_salary_hh(self, salary_str):
+        # self.salary = salary_str.replace('\u202f', '')
+        self.currency = salary_str.split()[-1]
+        if salary_str.find('–') != -1:
+            self.min_salary = int(salary_str.replace('\u202f', '').split()[0])
+            self.max_salary = int(salary_str.replace('\u202f', '').split()[2])
+        elif salary_str.find('от') != -1:
+            self.min_salary = int(salary_str.replace('\u202f', '').split()[1])
+            self.max_salary = None
+        elif salary_str.find('до') != -1:
+            self.min_salary = None
+            self.max_salary = int(salary_str.replace('\u202f', '').split()[1])
+        else:
+            self.min_salary = None
 
-all_vacancy = []
-for vacancy in vacancy_list:
-    vacancy_data = {}
-    name = vacancy.find('a', {'data-qa': 'vacancy-serp__vacancy-title'}).getText()
-    link = vacancy.find('a', {'class': 'bloko-link'})['href']
 
-    vacancy_data['name'] = name
-    vacancy_data['link'] = link
+class VacancyList:
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (HTML, like Gecko) Chrome/97.0.4692.71 '
+                      'Safari/537.36'}
 
-    all_vacancy.append(vacancy_data)
+    def __init__(self, vacancy_text, page=2):
+        self.all_vacancy = []
+        self.vacancy_text = vacancy_text
+        self.page_count = page
+        self.add_hh()
 
-pprint(all_vacancy)
+    def add_vacancy(self, vacancy):
+        self.all_vacancy.append(vacancy)
 
+    def add_hh(self):
+        domain = 'https://hh.ru'
+        url = '/search/vacancy'
+        params_dict = {'clusters': 'true',
+                       'area': 1,
+                       'enable_snippets': 'true',
+                       'st': 'searchVacancy',
+                       'text': self.vacancy_text,
+                       'page': 0}
+
+        for page_count in range(0, self.page_count):
+            params_dict['page'] = page_count
+            response = requests.get(domain + url, params=params_dict, headers=self.headers)
+            dom = BeautifulSoup(response.text, 'html.parser')
+
+            card_list = dom.find_all('div', {'class': 'vacancy-serp-item'})
+
+            for card in card_list:
+                vacancy = Vacancy()
+                vacancy.site = 'hh.ru'
+                vacancy.name = card.find('a', {'data-qa': 'vacancy-serp__vacancy-title'}).text
+                vacancy.domain = domain
+                vacancy.link = card.find('a', {'data-qa': 'vacancy-serp__vacancy-title'})['href']
+
+                try:
+                    salary = card.find('span', {'data-qa': 'vacancy-serp__vacancy-compensation'}).text
+                    vacancy.get_salary_hh(salary)
+
+                except AttributeError:
+                    vacancy.salary = None
+                    vacancy.min_salary = None
+                    vacancy.max_salary = None
+                    vacancy.currency = None
+
+                self.all_vacancy.append(vacancy)
+            print(f'Обработка страницы {page_count + 1}')
+            sleep(1)
+
+
+final_list = VacancyList(vacancy_text='Data scientist', page=3)
+
+vac_data = []
+for vacancy in final_list.all_vacancy:
+    vacancy_dict = {'name': vacancy.name,
+                    # 'site': vacancy.site,
+                    'link': vacancy.link,
+                    'max_salary': vacancy.max_salary,
+                    'min_salary': vacancy.min_salary,
+                    'currency': vacancy.currency
+                    }
+
+    vac_data.append(vacancy_dict)
+
+# with open('vacancy_data.txt', 'w') as f:
+#     for i in vac_data:
+#         f.write(str(i) + '\n')
+
+with open('vacancy_data.txt', 'r') as f:
+    print(f.read())
 
